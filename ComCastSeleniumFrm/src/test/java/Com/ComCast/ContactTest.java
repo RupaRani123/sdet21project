6b3adeb1848368428fe03d5bb4639759f856dc9f
RupaRani123 <ruparani110@gmail.com>
2021-09-22 12:29:20 +0530

    trasfer succesfully

---
+++ ComCastSeleniumFrm/src/test/java/Com/ComCast/ContactTest.java
@@ -0,0 +1,27 @@
+package Com.ComCast;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	@Test
+	
+ public void createContactTest()
+ {
+	System.out.println("execute createContactTest");
+	
+			 
+ }
+@Test
+public void createContactWithOrgTest() {
+	System.out.println("execute createContactWithOrgTest");
+	
+}
+@Test
+
+public void deleteContactTest()
+{
+System.out.println("execute deleteContactTest");
+
+}
+ 
+}
